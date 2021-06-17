b18ebd5474ce989c4561e8e242592f5f851196fc
fluttertests <nafakans@gmail.com>
2021-06-17 15:45:53 +0300

    Multiple imports deleted

--- lib/View/Widgets/LaunchDetails.dart
+++ lib/View/Widgets/LaunchDetails.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
 import 'package:intl/intl.dart';
 import 'package:spacex_latest_launch/Const.dart';
